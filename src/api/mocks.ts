11628a124b937bd89a78729b3bb28c55440067ab
Alex Lu <hellosimon1103@hotmail.com>
2024-12-22 13:39:48 +0800

    Add more mock data for category

--- src/api/mocks.ts
+++ src/api/mocks.ts
@@ -1,7 +1,79 @@
 import type { CategoryItem } from '@/types/home';
 
-export const mockCategoryItems = Array.from({ length: 15 }, (_, i) => ({
-  id: i + 1,
-  name: `Category ${i + 1}`,
-  icon: '/static/images/rice.png',
-})) as CategoryItem[];
+export const mockCategoryItems = <CategoryItem[]>[
+  {
+    id: 1,
+    name: '米面粮油',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 2,
+    name: '厨电厨具',
+    icon: '/static/images/category/kitchen.png',
+  },
+  {
+    id: 3,
+    name: '个户清洁',
+    icon: '/static/images/category/detergent.png',
+  },
+  {
+    id: 4,
+    name: '护肤美妆',
+    icon: '/static/images/category/cosmetics.png',
+  },
+  {
+    id: 5,
+    name: '休闲零食',
+    icon: '/static/images/category/crisps.png',
+  },
+  {
+    id: 6,
+    name: '肉禽蛋品',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 7,
+    name: '水产海鲜',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 8,
+    name: '时令蔬果',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 9,
+    name: '熟食面点',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 10,
+    name: '乳饮冲调',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 11,
+    name: '数码3C',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 12,
+    name: '生活电器',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 13,
+    name: '文体户外',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 14,
+    name: '营养保健',
+    icon: '/static/images/category/rice.png',
+  },
+  {
+    id: 15,
+    name: '家居生活',
+    icon: '/static/images/category/rice.png',
+  },
+];
